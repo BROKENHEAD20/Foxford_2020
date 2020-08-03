832752b0b17d5334ec39d6c817b4331fb4ffec53
Alex <try_vaper@mai.ru>
2020-08-03 19:30:23 +0500

    Заготовка

---
+++ building_house.py
@@ -0,0 +1,19 @@
+from graphics import *
+
+win_width = 600
+win_height = 500
+
+def main():
+    draw_house(win_width//3, win_height*2//3, 150, 200)
+
+def draw_house(x0, y0, width, height):
+    """
+    :param x0: центральная нижняя точка дома
+    :param y0: центральная нижняя точка дома
+    :param width: ширина
+    :param height: высота
+    :return:
+    """
+    pass
+
+main()
