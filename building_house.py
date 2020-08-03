dc61d8864113b54250334abb32264b67a06aad80
Alex <try_vaper@mai.ru>
2020-08-03 20:34:40 +0500

    Описано рисование домика, но функции - заглушки

--- building_house.py
+++ building_house.py
@@ -14,6 +14,28 @@ def draw_house(x0, y0, width, height):
     :param height: высота
     :return:
     """
+    foundation_height = int(height * 0.1)
+    walls_height = int(0.5 * height)
+    walls_width = int(0.9 * width)
+    roof_height = height - walls_height
+    window_height = walls_height // 3
+    window_width = walls_width // 3
+
+    draw_foundation(x0,y0, width, foundation_height)
+    draw_walls(x0, y0 - foundation_height, width, walls_height)
+    draw_window(x0, y0 - foundation_height - walls_height, window_height, window_width)
+    draw_roof(x0, y0-walls_height, width, roof_height)
+
+def draw_foundation(x0, y0, width, height):
     pass
+
+def draw_walls(x0, y0, width, height):
+    pass
+
+def draw_window(x0, y0, width, height):
+    pass
+
+def draw_roof(x0, y0, width, height):
+    pass    
 
 main()
